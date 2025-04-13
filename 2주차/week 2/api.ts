af877e3e009c4b7b7fa3ab4a6fc058f6127041a2
Handy <handylan18@gmail.com>
2025-04-13 15:36:17 +0900

    Update api.ts

--- 2주차/week 2/api.ts
+++ 2주차/week 2/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5000/',
+    baseURL: '[로컬호스트 주소]',
     timeout: 1000,
     headers: {},
     withCredentials: true
